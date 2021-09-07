7747214009434cc6243baaf409155686d8280e2f
alfinanur17 <alfinanur0117@gmail.com>
2021-09-07 11:36:05 +0700

    menghapus

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/', function ($id) {
+Route::get('/', function () {
     echo "Selamat Datang";
 });
